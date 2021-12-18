2fde80945eedc8fc900014e19e69346ccdca0b9a
Rahul Nirwan <rahulnirwan44@gmail.com>
2021-12-18 14:24:35 +0530

    update index.js

--- index.js
+++ index.js
@@ -13,22 +13,108 @@ app.get("/",(req,res)=>{
     })
 
 
-app.post("/student",(req,res)=>{
+app.post("/student",async (req,res)=>{
 
-    console.log(req.body)
-    const user=student(req.body)
-    user.save().then(()=>{
+            try{
+            console.log(req.body)
+            const user= new student(req.body)
+            const Createuser=await user.save()
+                res.status(201).send(Createuser)
+            }
 
-        res.status(201).send(user)
-    }).catch((err)=>{
-        // console.log(`Error while creating student detail : ${err}`);
-    res.status(400).send(err)
+            catch (err) {
+                res.status(400).send(err)
+        
+            }
+        
     })
 
+    // read the data from reegistred student
+
+    app.get('/student',async (req,res)=>{
+
+        try {
+
+            const studentsData= await student.find();
+            res.send(studentsData);
+            
+        } catch (error) {
+
+            res.send(error)
+            
+        }
 
 
-// res.send("hello small world");
+
+    })
+// get the individual student data using id
+
+app.get("/student/:id",async (req,res)=>{
+
+    try {
+
+        const _id = req.params.id;
+        const studentdata=await student.findById({_id})
+        res.send(studentdata);
+    if(!studentdata){
+        return res.status(404).send();
+    }
+    else{
+        res.send(studentdata)
+    }
+    
+} 
+
+    catch (error) {
+        res.send(error)
+    }
+
 })
+
+
+// update the student by it id
+
+app.patch("/student/:id", async(req,res)=>{
+
+        try {
+            const _id= req.params.id;
+            const updateStudents= await student.findByIdAndUpdate(_id,req.body,{
+            new:true
+            })
+            res.send(updateStudents)            
+        } 
+        catch (error) {
+            res.send(error)
+        }
+
+})
+
+
+// delete student by its id
+
+
+app.delete('/student/:id',async(req,res)=>{
+
+    try {
+        const _id= req.params.id;
+        const deleteStudents= await student.findByIdAndDelete(_id)
+
+        if(!req.params.id){
+
+            return res.status(400).send()
+
+        }
+
+        res.send(deleteStudents)
+        
+    } catch (error) {        
+        res.status(500).send(error)
+    }
+})
+
+
+
+
 
 app.listen(port,function(err){
 
